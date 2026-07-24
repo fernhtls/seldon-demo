8c52b6b3f2a1b415298fb9ca399ecf0c0277ab3b
Fernando Souza <fernhtls@gmail.com>
2026-07-24 22:44:12 +0200

    tf adjustments and small changes

--- infra/deploy-models/meta-kubernetes.tf
+++ infra/deploy-models/meta-kubernetes.tf
@@ -2,3 +2,9 @@ provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube-fsouza1"
 }
+
+provider "kubectl" {
+  apply_retry_count = 3 # number of attempts any create/update action will take.
+  config_path       = "~/.kube/config"
+  config_context    = "minikube-fsouza1"
+}
