2309170079518f9e126302d91f51725976c58f13
Fernando Souza <fernhtls@gmail.com>
2026-07-25 11:02:00 +0200

    using locals on tf for some values

--- infra/deploy-models/meta-kubernetes.tf
+++ infra/deploy-models/meta-kubernetes.tf
@@ -1,10 +1,16 @@
-provider "kubernetes" {
+locals {
+  # Change the k8s config
   config_path    = "~/.kube/config"
   config_context = "minikube-fsouza1"
 }
 
+provider "kubernetes" {
+  config_path    = local.config_path
+  config_context = local.config_context
+}
+
 provider "kubectl" {
   apply_retry_count = 3 # number of attempts any create/update action will take.
-  config_path       = "~/.kube/config"
-  config_context    = "minikube-fsouza1"
+  config_path       = local.config_path
+  config_context    = local.config_context
 }
